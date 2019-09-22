b8e3ef6779214f46e294f3c4e57bf64030b89f3c
Nikita Pilinko <mineralsfree@gmail.com>
2019-09-22 14:13:20 +0300

    123123

---
+++ ConsoleApp1/Mutex.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    public class Mutex
+    {
+        private int value;    
+       public Mutex(int intialValue)
+        {
+            if (intialValue > 1 || intialValue < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            this.value = intialValue;
+        }
+
+       public void Unlock()
+       {
+           while (Interlocked.CompareExchange(ref this.value, 0, 1) == 0)
+           {
+               Thread.Sleep(10);
+           } 
+       }
+
+       public void Lock()
+       {
+           while (Interlocked.CompareExchange(ref this.value, 1, 0) == 1)
+           {
+               Thread.Sleep(10);
+           }
+       }
+    }
+}
